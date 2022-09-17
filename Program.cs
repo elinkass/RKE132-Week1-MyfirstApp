092a1017b0a156c5c7c5c0607a4c0aeba5f2be41
elinkass <elinkass@tktk.ee>
2022-09-17 10:40:42 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+
+string username = Console.ReadLine();
+
+Console.WriteLine("HELLO" + ", "+username+"!");
+
+
+Console.WriteLine($"Hello, {username} !");
